621e7e33395ee9132139ec01d71e09669a513111
santoshi-1 <karuturisantoshi@gmail.com>
2023-08-12 19:05:00 -0400

    Added additional support of frames and csrf to spring security

--- src/main/java/com/santoshi/springboot/todoapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/santoshi/springboot/todoapp/security/SpringSecurityConfiguration.java
@@ -3,11 +3,14 @@ package com.santoshi.springboot.todoapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -30,6 +33,34 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Features of spring security -
+     * 1. All URL's are protected
+     * 2. A login form is shown for un-authorized requests
+     * <p>
+     * Add additional functionality -
+     * 1. CSRF disabled
+     * 2. Frames
+     */
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        /**
+         * ensuring all requests are authenticated.
+         */
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        /**
+         * disabling csrf and enabling use of frames in the application
+         */
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.headers(frameOptions -> frameOptions.disable());
+        return httpSecurity.build();
     }
 
 }
